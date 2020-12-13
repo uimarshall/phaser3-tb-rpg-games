97971b1fcc7b8a417f003968d436a766789b7a74
uimarshall <uimarshall@gmail.com>
2020-12-13 11:33:03 +0100

    Add player,player physics for anims and collisions

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -14,6 +14,7 @@ class GameScene extends Phaser.Scene {
     this.add.image(400, 300, 'sky');
     // this.add.spritesheet(400, 300, 'tileset');
     this.createPlatforms()
+    this.createPlayer()
   }
 
   createPlatforms(){
@@ -25,6 +26,36 @@ class GameScene extends Phaser.Scene {
     this.platforms.create(600, 400, 'platform');
     this.platforms.create(50, 250, 'platform');
     this.platforms.create(750, 220, 'platform')
+  }
+
+  createPlayer(){
+    this.player = this.physics.add.sprite(100, 450, 'hero');
+
+this.player.setBounce(0.2);
+this.player.setCollideWorldBounds(true);
+
+// Add collision btw player and platforms
+this.physics.add.collider(this.player, this.platforms);
+
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
+
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'hero', frame: 4 } ],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('hero', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
   }
 }
 
