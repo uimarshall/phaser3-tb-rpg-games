f6aa31d0f8469ed2b51b9e31e24de6ec30ff1c09
uimarshall <uimarshall@gmail.com>
2020-12-13 01:38:19 +0100

    Add logic to the PreloaderScene

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import Game from './app/app';
 window.game = new Game();
 // Globals
 
-window.game.URL = '';
-window.game.CONFIG = {
-  width: config.width,
-  height: config.height,
-  centerX: Math.round(0.5 * config.width),
-  centerY: Math.round(0.5 * config.height),
-  tile: 32, // size in px of each individual tile
-  mapOffset: 4,
-};
+// window.game.URL = '';
+// window.game.CONFIG = {
+//   width: config.width,
+//   height: config.height,
+//   centerX: Math.round(0.5 * config.width),
+//   centerY: Math.round(0.5 * config.height),
+//   tile: 32, // size in px of each individual tile
+//   mapOffset: 4,
+// };
 
 // Sound
 window.game.soundOn = true;
--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -13,14 +13,103 @@ class PreloaderScene extends Phaser.Scene {
     this.CONFIG = this.sys.game.CONFIG;
   }
 
+  ready () {
+  this.readyCount++;
+  if (this.readyCount === 2) {
+    this.scene.start('Title');
+  }
+}
+
   preload() {
+
+  // load assets needed in our game
+  this.load.image('blueButton1', 'src/assets/images/blue_btn02.png');
+  this.load.image('blueButton2', 'src/assets/images/blue_btn03.png');
+//   this.load.image('phaserLogo', 'assets/logo.png');  
+
+    // display progress bar
+  var progressBar = this.add.graphics();
+  var progressBox = this.add.graphics();
+  progressBox.fillStyle(0x222222, 0.8);
+  progressBox.fillRect(240, 270, 320, 50);
+ 
+  var width = this.cameras.main.width;
+  var height = this.cameras.main.height;
+  var loadingText = this.make.text({
+    x: width / 2,
+    y: height / 2 - 50,
+    text: 'Loading...',
+    style: {
+      font: '20px monospace',
+      fill: '#ffffff'
+    }
+  });
+  loadingText.setOrigin(0.5, 0.5);
+ 
+  var percentText = this.make.text({
+    x: width / 2,
+    y: height / 2 - 5,
+    text: '0%',
+    style: {
+      font: '18px monospace',
+      fill: '#ffffff'
+    }
+  });
+  percentText.setOrigin(0.5, 0.5);
+ 
+  var assetText = this.make.text({
+    x: width / 2,
+    y: height / 2 + 50,
+    text: '',
+    style: {
+      font: '18px monospace',
+      fill: '#ffffff'
+    }
+  });
+  assetText.setOrigin(0.5, 0.5);
+ 
+  // update progress bar
+  this.load.on('progress', function (value) {
+    percentText.setText(parseInt(value * 100) + '%');
+    progressBar.clear();
+    progressBar.fillStyle(0xffffff, 1);
+    progressBar.fillRect(250, 280, 300 * value, 30);
+  });
+ 
+  // update file progress text
+  this.load.on('fileprogress', function (file) {
+    assetText.setText('Loading asset: ' + file.key);
+  });
+ 
+  // remove progress bar when complete
+  this.load.on('complete', function () {
+    progressBar.destroy();
+    progressBox.destroy();
+    loadingText.destroy();
+    percentText.destroy();
+    assetText.destroy();
+  });
+
+  // remove progress bar when complete
+this.load.on('complete', function () {
+  progressBar.destroy();
+  progressBox.destroy();
+  loadingText.destroy();
+  percentText.destroy();
+  assetText.destroy();
+  this.ready();
+}.bind(this));
+ 
+this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
+ 
+ 
+  
     // Load Background
 //     this.bg = this.add.graphics({ x: 0, y: 0 });
 //     this.bg.fillStyle('0xD6CDB0', 1);
 //     this.bg.fillRect(0, 0, this.CONFIG.width, this.CONFIG.height);
 //     // Create loading bar
 //     this.createLoader();
-//     // Set path to where spritesheets are located
 //     this.load.setPath(`${this.URL}src/assets/images`);
 
 //     // Spritesheets - include only key and file-name
