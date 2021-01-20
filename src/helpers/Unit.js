f127ea241311d382e356e0c6971dc4367b93494a
uimarshall <uimarshall@gmail.com>
2021-01-20 12:50:28 +0100

    Fix linting errors

--- src/helpers/Unit.js
+++ src/helpers/Unit.js
@@ -1,41 +1,41 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 // We will make our units fight the enemies!
 
 const Unit = new Phaser.Class({
-	Extends: Phaser.GameObjects.Sprite,
+  Extends: Phaser.GameObjects.Sprite,
 
-	initialize: function Unit(scene, x, y, texture, frame, type, hp, damage) {
-		Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame);
-		this.type = type;
-		this.maxHp = hp;
-		this.hp = hp;
-		this.damage = damage;
-		this.living = true;
-		this.menuItem = null;
-	},
-	setMenuItem(item) {
-		this.menuItem = item;
-	},
-	attack(target) {
-		if (target.living) {
-			target.takeDamage(this.damage);
-			this.scene.events.emit(
-				"Message",
-				`${this.type} attacks ${target.type} for ${this.damage} damage`
-			);
-		}
-	},
-	takeDamage(damage) {
-		this.hp -= damage;
-		if (this.hp <= 0) {
-			this.hp = 0;
-			this.menuItem.unitKilled();
-			this.living = false;
-			this.visible = false;
-			this.menuItem = null;
-			this.healthBar.rem();
-		}
-	},
+  initialize: function Unit(scene, x, y, texture, frame, type, hp, damage) {
+    Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame);
+    this.type = type;
+    this.maxHp = hp;
+    this.hp = hp;
+    this.damage = damage;
+    this.living = true;
+    this.menuItem = null;
+  },
+  setMenuItem(item) {
+    this.menuItem = item;
+  },
+  attack(target) {
+    if (target.living) {
+      target.takeDamage(this.damage);
+      this.scene.events.emit(
+        'Message',
+        `${this.type} attacks ${target.type} for ${this.damage} damage`,
+      );
+    }
+  },
+  takeDamage(damage) {
+    this.hp -= damage;
+    if (this.hp <= 0) {
+      this.hp = 0;
+      this.menuItem.unitKilled();
+      this.living = false;
+      this.visible = false;
+      this.menuItem = null;
+      this.healthBar.rem();
+    }
+  },
 });
 
 export default Unit;
--- src/scenes/UserScene.js
+++ src/scenes/UserScene.js
@@ -1,68 +1,68 @@
 /* eslint-disable no-undef */
 /* eslint-disable import/no-cycle */
-import Phaser from "phaser";
-import config from "../app/config";
+/* eslint-disable no-tabs */
+import Phaser from 'phaser';
+import config from '../app/config';
 
 export default class UserScene extends Phaser.Scene {
-	constructor() {
-		super("UserScene");
-	}
+  constructor() {
+    super('UserScene');
+  }
 
-	init() {
-		this.scale.fullscreenTarget = document.getElementById(config.parent);
-	}
+  init() {
+    this.scale.fullscreenTarget = document.getElementById(config.parent);
+  }
 
-	create() {
-		this.back = this.add.image(400, 300, "background");
-		this.add.image(400, 100, "title");
+  create() {
+    this.back = this.add.image(400, 300, 'background');
+    this.add.image(400, 100, 'title');
 
-		// try to add the story
-		const story = [
-			"Your league of spies just received an intel that the palace of Condor is about to be attacked by witches of Endor in 3 weeks. You have to be there to protect Condor.",
+    // try to add the story
+    const story = [
+      'Your league of spies just received an intel that the palace of Condor is about to be attacked by witches of Endor in 3 weeks. You have to be there to protect Condor.',
 
-			"The people of Condor are relying on you to save them. Do not fail your mission.",
-		];
+      'The people of Condor are relying on you to save them. Do not fail your mission.',
+    ];
 
-		const graphics = this.make.graphics();
+    const graphics = this.make.graphics();
 
-		graphics.fillRect(70, 160, 665, 270);
-		const mask = new Phaser.Display.Masks.GeometryMask(this, graphics);
-		const text = this.add.text(70, 160, story, {
-			color: "white",
-			fontSize: "18px ",
-			wordWrap: { width: 665 },
-			align: "center",
-		});
-		text.setMask(mask);
+    graphics.fillRect(70, 160, 665, 270);
+    const mask = new Phaser.Display.Masks.GeometryMask(this, graphics);
+    const text = this.add.text(70, 160, story, {
+      color: 'white',
+      fontSize: '18px ',
+      wordWrap: { width: 665 },
+      align: 'center',
+    });
+    text.setMask(mask);
 
-		// end of story
+    // end of story
 
-		this.add.text(270, 430, "Please enter your name", {
-			color: "white",
-			fontSize: "18px ",
-		});
-		this.add.text(700, 560, "v.1.0.7", {
-			color: "#fff",
-			fontSize: "12px",
-		});
+    this.add.text(270, 430, 'Please enter your name', {
+      color: 'white',
+      fontSize: '18px ',
+    });
+    this.add.text(700, 560, 'v.1.0.7', {
+      color: '#fff',
+      fontSize: '12px',
+    });
 
-		const input = this.add.dom(400, 478, "input", {
-			type: "text",
-			name: "nameField",
-			fontSize: "32px",
-		});
+    const input = this.add.dom(400, 478, 'input', {
+      type: 'text',
+      name: 'nameField',
+      fontSize: '32px',
+    });
 
-		const style =
-			"background: url(assets/images/btn_sm.png); width: 490px; height: 77px; border: none; font: 32px Georgia; color: #fff;";
-		const btn = this.add.dom(390, 550, "button", style, "Play Now");
-		btn.addListener("click");
+    const style =			'background: url(assets/images/btn_sm.png); width: 490px; height: 77px; border: none; font: 32px Georgia; color: #fff;';
+    const btn = this.add.dom(390, 550, 'button', style, 'Play Now');
+    btn.addListener('click');
 
-		btn.on("click", () => {
-			if (input.node.value) {
-				this.model = this.sys.game.globals.model;
-				this.model.userName = input.node.value;
-				this.scene.start("Title");
-			}
-		});
-	}
+    btn.on('click', () => {
+      if (input.node.value) {
+        this.model = this.sys.game.globals.model;
+        this.model.userName = input.node.value;
+        this.scene.start('Title');
+      }
+    });
+  }
 }
